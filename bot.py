0df15e80a16b5b629ce6a6ddcdc03e6cf75b5e3b
Alfredo Lobaina <93672385+lobaina7@users.noreply.github.com>
2022-12-06 22:25:35 -0500

    Update the bot

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ def qr_callback_handler(update,context):
 
 if __name__== '__main__': #la funcion empezaria por aqui
     #updater: para saber las peticiones que va recibiendo el bot de parte del usuario
-    updater = Updater(token='5058818013:AAGkn-UNjkngHrVn8fbuQjUmZ145G7YWKBw', use_context=True)#la nueva API de python pide la variable use_context
+    updater = Updater(token='xxxxxx', use_context=True)#la nueva API de python pide la variable use_context
     #dispatcher: se encarga de nviar las acciones, lo que entra por updater pasa por el dispatcher
     dp=updater.dispatcher
     #add handler: handler es una funcion para ejecutar
